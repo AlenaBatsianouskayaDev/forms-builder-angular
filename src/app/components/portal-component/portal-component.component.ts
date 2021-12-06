b4e8389a3ff2baf9553cefe3b7c436ff87c03e08
Alena Botyanovskaya <95024837+AlenaBatsianouskayaDev@users.noreply.github.com>
2021-12-06 16:37:51 +0200

    add draft drug&drop function

--- src/app/components/portal-component/portal-component.component.ts
+++ src/app/components/portal-component/portal-component.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-portal-component',
   templateUrl: './portal-component.component.html',
   styleUrls: ['./portal-component.component.scss']
 })
-export class PortalComponentComponent implements OnInit {
+  
+export class PortalComponentComponent {
+  todo = ["<app-input>"];
 
-  constructor() { }
+  done = [];
 
-  ngOnInit(): void {
+  drop(event: CdkDragDrop<any>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
   }
-
 }
--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -3,12 +3,14 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSelectModule } from '@angular/material/select';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 const MaterialComponents = [
   MatButtonModule,
   MatInputModule,
   MatCheckboxModule,
   MatSelectModule,
+  DragDropModule,
 ]
 
 @NgModule({
