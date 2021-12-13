233282315a7cdfa93fc86d33e86a27ab30fe43cf
Alena Botyanovskaya <95024837+AlenaBatsianouskayaDev@users.noreply.github.com>
2021-12-13 11:06:54 +0200

    chahged class User(auth) to interface User(auth)

--- src/app/models/user.models.ts
+++ src/app/models/user.models.ts
@@ -1,13 +1,4 @@
-export class User {
-  constructor(public username: string, public password:string){}
+export interface User {
+  username: string,
+  password: string
 }
-
-// export interface Authenticate {
-//   username: string,
-//   password: string
-// }
-
-// export interface User {
-//   name: string,
-//   token: string
-// }
